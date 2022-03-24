ea0b62463cc85a2ad67ead8c8d5b00b27d9daa8e
Oséias <oseas-ramos123@hotmail.com>
2022-03-24 13:42:52 -0300

    first commit

---
+++ src/main/java/com/exemplo/api/CreateDto.java
@@ -0,0 +1,24 @@
+package com.exemplo.api;
+
+public class CreateDto {
+	
+	private String email;
+	private int numero;
+	
+	
+	
+
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getNumber() {
+		return numero;
+	}
+	public void setNumber(int numero) {
+		this.numero = numero;
+	}
+
+}
---
+++ src/main/java/com/exemplo/api/RegistrationService.java
@@ -0,0 +1,63 @@
+package com.exemplo.api;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+//mesmo metodos controller 
+
+@Service
+public class RegistrationService {
+	
+	@Autowired
+	RegistrationRepository regRepository;
+	
+	
+	public VacancyModel create(CreateDto obj) {
+		VacancyModel vc = new VacancyModel();
+		vc.setEmail(obj.getEmail());
+		vc.setNumber(obj.getNumber());
+		return regRepository.save(vc);
+	}
+
+	
+	
+	public VacancyModel update(long id, CreateDto newData) {
+
+		Optional<VacancyModel> vm = regRepository.findById(id);
+		vm.get().setNumber(newData.getNumber());
+		return regRepository.save(vm.get());
+		
+	}
+	
+	
+	
+	
+	
+	public VacancyModel read(String email) {
+		
+		 return regRepository.findByEmail(email);
+		
+		
+		
+	}
+	
+	
+	
+	
+	public void delete(long id) {
+		Optional<VacancyModel> vm = regRepository.findById(id);
+		regRepository.delete(vm.get()); 
+	}
+	
+	
+	
+	
+	
+}
+
+
+
+
